bf6fda81408608f375226afc50f2cd3cc7b5255f
Tomasz Jaeschke <jaeschke.tomasz@gmail.com>
2021-04-06 18:50:41 +0200

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Tomasz Jaeschke on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
