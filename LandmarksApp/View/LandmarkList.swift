bd0bdbdc58603d53ef6295a7884db53f65be9120
Tomasz Jaeschke <jaeschke.tomasz@gmail.com>
2021-04-12 07:02:32 +0200

    Set up navigation - in progress

---
+++ LandmarksApp/View/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  LandmarksApp
+//
+//  Created by Tomasz Jaeschke on 12/04/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
